0886a6ddd628a76ed1264a4760655a8bfd97b149
vaporbolt <vaporbolt@gmail.com>
2021-03-30 14:36:36 -0400

    InputField now has a method that checks for valid characters including +, i, space, and -.

--- rimplex/src/gui/InputFieldDocumentFilter.java
+++ rimplex/src/gui/InputFieldDocumentFilter.java
@@ -1,23 +1,40 @@
 package gui;
 
 import java.awt.Toolkit;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DocumentFilter;
-import javax.swing.text.Style;
-import javax.swing.text.StyleConstants;
-import javax.swing.text.StyleContext;
+
 
 /**
  * @author Seth Roper
  * @version 3/29/2020
  * document filter for the input field.
  * prevents characters to be entered after the i has been inputted.
+ * 
  *
  */
 public class InputFieldDocumentFilter extends DocumentFilter
 {
+  private String i = "i";
+  private ArrayList<String> validChars;
+  // dumb checkstyle thing.
+  
+  /**
+   * Constructs an InputFiledDocumentFIlter.
+   */
+  public InputFieldDocumentFilter()
+  {
+    super();
+    validChars = new ArrayList<String>();
+    validChars.add("+");
+    validChars.add("-");
+    validChars.add(" ");
+    validChars.add(i);
+  }
   
   @Override
   public void insertString(final DocumentFilter.FilterBypass fb, final int offset,
@@ -25,9 +42,9 @@ public class InputFieldDocumentFilter extends DocumentFilter
   {
 
     // if the document has an i, don't insert the update.
-    if (!fb.getDocument().getText(0, fb.getDocument().getLength()).contains("i"))
+    if (!fb.getDocument().getText(0, fb.getDocument().getLength()).contains(i))
     {
-      if (string.equals("i"))
+      if (string.equals(i))
         super.insertString(fb, offset, string, TypesettingStyle.applyTypesetting());
       else
         super.insertString(fb, offset, string, null);
@@ -42,24 +59,27 @@ public class InputFieldDocumentFilter extends DocumentFilter
   public void replace(final DocumentFilter.FilterBypass fb, final int offset, final int length,
       final String string, final AttributeSet attr) throws BadLocationException
   {
-    
-    if (!string.equals(" ") && !string.equals("+") && !string.equals("i"))
-      // if it's isnt a number, make an error
-      try
-      {
-        Integer.parseInt(string);
-      }
+  
+    try
+    {
+      Integer.parseInt(string);
+    }
 
-      catch (NumberFormatException e)
+    catch (NumberFormatException e)
+    {
+      if (!this.isValidInput(string))
       {
         Toolkit.getDefaultToolkit().beep();
         return;
       }
+    }
+
+  
     // if the document has an i, don't insert the update.
     if (fb.getDocument().getText(0, fb.getDocument().getLength()) != null
-        && !fb.getDocument().getText(0, fb.getDocument().getLength()).contains("i"))
+        && !fb.getDocument().getText(0, fb.getDocument().getLength()).contains(i))
     {
-      if (string.equals("i"))
+      if (string.equals(i))
         super.replace(fb, offset, length, string, TypesettingStyle.applyTypesetting());
       else
         super.replace(fb, offset, length, string, null);
@@ -68,5 +88,25 @@ public class InputFieldDocumentFilter extends DocumentFilter
     {
       Toolkit.getDefaultToolkit().beep();
     }
+  }
+  
+  /**
+   * @return whether the character entered is a valid character of an imaginary number.
+   * @param string the string whose valid input will be determined.
+   * 
+   */
+  private boolean isValidInput(final String string) 
+  {
+    Iterator<String> iterator = this.validChars.iterator();
+    boolean isValid = false;
+    while(iterator.hasNext())
+    {
+      if(string.equals(iterator.next()))
+      {
+        isValid = true;
+      }
+    }
+    
+    return isValid;
   }
 }
