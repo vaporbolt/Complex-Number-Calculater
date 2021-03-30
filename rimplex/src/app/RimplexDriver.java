8e6021729b696b5e444f81c4c839eb5d28a09e90
Jackson Brantley <Jackson Brantley@DESKTOP-25H45P6.fios-router.home>
2021-03-29 22:16:54 -0400

    Added comments to TypesettingStyle for display

--- rimplex/src/app/RimplexDriver.java
+++ rimplex/src/app/RimplexDriver.java
@@ -72,16 +72,31 @@ public class RimplexDriver
         // display result
         display.addComplexNumber(result);
         
-        // handle Typesetting style for i
+        // replaces all i characters in display with italicized i characters
         try
         {
+          // copy the text from display into placeholder string
           String s = display.getPanel().getDocument().getText(0, display.getPanel().getDocument().getLength());
+          
+          // length of the text that has been removed from string s, for indexing purposes
           int length = 0;
+          
+          // continue looping over string s while there are still i characters
           while (s.contains("i"))
           {
-            display.getPanel().getDocument().remove(s.indexOf("i") + length, 1);
-            display.getPanel().getDocument().insertString(s.indexOf("i") + length, "i", TypesettingStyle.applyTypesetting());
+            // sets the current index to the next i character in display
+            int index = s.indexOf("i") + length;
+            
+            // removes the un-italic i from display
+            display.getPanel().getDocument().remove(index, 1);
+            
+            // adds the now italicized i to the location the un-italic i was in
+            display.getPanel().getDocument().insertString(index, "i", TypesettingStyle.applyTypesetting());
+            
+            // sets length to the length of text that has been removed from string s
             length += s.substring(0, s.indexOf("i") + 1).length();
+            
+            // removes everything in the string before and including the current i character
             s = s.substring(s.indexOf("i") + 1);
           }
         }
