d13deea32e5b647c70e2240fbdb19f806923fdbd
vaporbolt <vaporbolt@gmail.com>
2021-03-31 16:52:20 -0400

    Added skeleton handlers for all of the buttons

---
+++ rimplex/src/gui/MultiplicationHandler.java
@@ -0,0 +1,22 @@
+package gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @author Seth Roper
+ * @version 3/31/2021
+ * Handles actions for the addition button.
+ *
+ */
+public class MultiplicationHandler implements ActionListener
+{
+
+  @Override
+  public void actionPerformed(final ActionEvent e)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
