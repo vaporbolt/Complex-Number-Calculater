5651b5bd717704fcdb64dd821e683c6a01728b87
Owner <Owner@DESKTOP-AQGDVQS>
2021-03-30 23:49:28 -0400

    Added Unit tests for Operation. Added factory methods to ComplexNumber.

--- rimplex/src/math/ComplexNumber.java
+++ rimplex/src/math/ComplexNumber.java
@@ -1,7 +1,8 @@
 package math;
 
 /**
- * ComplexNumber class.
+ * ComplexNumber class. It can represent either a real number, an imaginary number, or a real number
+ * plus an imaginary number
  * 
  * @author Sebastien Roy
  *
@@ -23,6 +24,30 @@ public class ComplexNumber
   {
     this.real = real;
     this.imaginary = imaginary;
+  }
+
+  /**
+   * Factory method that creates a complex number that represents a real number.
+   * 
+   * @param real
+   *          the value of the real number
+   * @return a new Complex Number
+   */
+  public static ComplexNumber constructReal(final double real)
+  {
+    return new ComplexNumber(real, 0.0);
+  }
+
+  /**
+   * Factory method that creates a complex number that represents a imaginary number.
+   * 
+   * @param imaginary
+   *          the value of the imaginary number
+   * @return a new Complex Number
+   */
+  public static ComplexNumber constructImaginary(final double imaginary)
+  {
+    return new ComplexNumber(0.0, imaginary);
   }
 
   /**
@@ -68,21 +93,23 @@ public class ComplexNumber
   {
     return new ComplexNumber(this.real, this.imaginary * -1);
   }
-  
+
   /**
    * return a complex number with parentheses around it.
+   * 
    * @return String
    */
   public String toString()
   {
     return toString(true);
   }
-  
+
   /**
-   * Converts the complex number into a string. 
-   * If either the real or imaginary number is an integer it will be displayed without the decimal.
+   * Converts the complex number into a string. If either the real or imaginary number is an integer
+   * it will be displayed without the decimal.
    * 
-   * @param parentheses if true add parentheses around the complex number
+   * @param parentheses
+   *          if true add parentheses around the complex number
    * @return the string form of a complex number.
    */
   public String toString(boolean parentheses)
@@ -90,20 +117,24 @@ public class ComplexNumber
     String complete;
     String realNum = "";
     String imaginaryNum = "";
-    
+
     // check for integer or double
-    if (this.real == (int)this.real) realNum += (int)this.real;
-    else realNum += this.real;
-    
+    if (this.real == (int) this.real)
+      realNum += (int) this.real;
+    else
+      realNum += this.real;
+
     // check for integer or double
-    if (this.imaginary == (int)this.imaginary) imaginaryNum += (int)this.imaginary;
-    else imaginaryNum += this.imaginary;
-    
+    if (this.imaginary == (int) this.imaginary)
+      imaginaryNum += (int) this.imaginary;
+    else
+      imaginaryNum += this.imaginary;
+
     if (parentheses)
     {
       complete = "(" + realNum + " + " + imaginaryNum + "i)";
     }
-    else 
+    else
     {
       complete = realNum + " + " + imaginaryNum + "i";
     }
--- rimplex/src/testing/OperationTest.java
+++ rimplex/src/testing/OperationTest.java
@@ -17,7 +17,7 @@ class OperationTest
 {
 
   @Test
-  void testAdd()
+  void testAddTwoComplex()
   {
     ComplexNumber num1 = new ComplexNumber(2, 3);
     ComplexNumber num2 = new ComplexNumber(4, 2);
@@ -37,7 +37,7 @@ class OperationTest
   }
 
   @Test
-  void testSubtract()
+  void testSubtractTwoComplex()
   {
     ComplexNumber num1 = new ComplexNumber(5, 5);
     ComplexNumber num2 = new ComplexNumber(3, 2);
@@ -57,7 +57,7 @@ class OperationTest
   }
 
   @Test
-  void testMultiply()
+  void testMultiplyTwoComplex()
   {
     ComplexNumber num1 = new ComplexNumber(5, 5);
     ComplexNumber num2 = new ComplexNumber(3, 2);
@@ -101,7 +101,7 @@ class OperationTest
   }
 
   @Test
-  void testDevide()
+  void testDevideTwoComplex()
   {
     ComplexNumber num1 = new ComplexNumber(5, 5);
     ComplexNumber num2 = new ComplexNumber(3, 2);
@@ -138,6 +138,128 @@ class OperationTest
     ComplexNumber num5 = new ComplexNumber(0, 0);
 
     assertThrows(IllegalArgumentException.class, () -> Operation.divide(num4, num5));
+  }
+
+  @Test
+  void testAddTwoReal()
+  {
+    ComplexNumber num1 = new ComplexNumber(2, 0);
+    ComplexNumber num2 = new ComplexNumber(4, 0);
+    ComplexNumber num3 = Operation.add(num1, num2);
+
+    assertEquals(6, num3.getReal());
+
+  }
+
+  @Test
+  void testSubtractTwoReal()
+  {
+    ComplexNumber num1 = new ComplexNumber(5, 0);
+    ComplexNumber num2 = new ComplexNumber(3, 0);
+    ComplexNumber num3 = Operation.subtract(num1, num2);
+
+    assertEquals(2, num3.getReal());
+
+  }
+
+  @Test
+  void testMultiplyTwoReal()
+  {
+    ComplexNumber num1 = new ComplexNumber(5, 0);
+    ComplexNumber num2 = new ComplexNumber(3, 0);
+    ComplexNumber num3 = Operation.multiply(num1, num2);
+
+    assertEquals(15, num3.getReal());
+
+    num1 = new ComplexNumber(-5, 0);
+    num2 = new ComplexNumber(-3, 0);
+
+    num3 = Operation.multiply(num1, num2);
+
+    assertEquals(15, num3.getReal());
+
+  }
+
+  @Test
+  void testDevideTwoReal()
+  {
+    ComplexNumber num1 = new ComplexNumber(5, 0);
+    ComplexNumber num2 = new ComplexNumber(3, 0);
+    ComplexNumber num3 = Operation.divide(num1, num2);
+
+    assertEquals(5.0 / 3, num3.getReal());
+
+    num1 = new ComplexNumber(-5, 0);
+    num2 = new ComplexNumber(-3, 0);
+
+    num3 = Operation.divide(num1, num2);
+
+    assertEquals(5.0 / 3, num3.getReal());
+
+  }
+
+  @Test
+  void testAddOneRealOneComplex()
+  {
+    ComplexNumber num1 = new ComplexNumber(2, 3);
+    ComplexNumber num2 = new ComplexNumber(4, 0);
+    ComplexNumber num3 = Operation.add(num1, num2);
+
+    assertEquals(6, num3.getReal());
+    assertEquals(3, num3.getImaginary());
+
+  }
+
+  @Test
+  void testSubtratOneRealOneComplex()
+  {
+    ComplexNumber num1 = new ComplexNumber(5, 3);
+    ComplexNumber num2 = new ComplexNumber(3, 0);
+    ComplexNumber num3 = Operation.subtract(num1, num2);
+
+    assertEquals(2, num3.getReal());
+    assertEquals(3, num3.getImaginary());
+
+  }
+
+  @Test
+  void testMultiplyOneRealOneComplex()
+  {
+    ComplexNumber num1 = new ComplexNumber(5, 3);
+    ComplexNumber num2 = new ComplexNumber(3, 0);
+    ComplexNumber num3 = Operation.multiply(num1, num2);
+
+    assertEquals(15, num3.getReal());
+    assertEquals(9, num3.getImaginary());
+
+    num1 = new ComplexNumber(-5, 3);
+    num2 = new ComplexNumber(-3, 0);
+
+    num3 = Operation.multiply(num1, num2);
+
+    assertEquals(15, num3.getReal());
+    assertEquals(-9, num3.getImaginary());
+
+  }
+
+  @Test
+  void testDevideOneRealOneComplex()
+  {
+    ComplexNumber num1 = new ComplexNumber(5, 3);
+    ComplexNumber num2 = new ComplexNumber(3, 0);
+    ComplexNumber num3 = Operation.divide(num1, num2);
+
+    assertEquals(5.0 / 3, num3.getReal());
+    assertEquals(1.0, num3.getImaginary());
+
+    num1 = new ComplexNumber(-5, 3);
+    num2 = new ComplexNumber(-3, 0);
+
+    num3 = Operation.divide(num1, num2);
+
+    assertEquals(5.0 / 3, num3.getReal());
+    assertEquals(-1.0, num3.getImaginary());
+
   }
 
 }
