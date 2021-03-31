e133ca3d01da1347faf4117e42cfaa99bfd84295
Owner <Owner@DESKTOP-AQGDVQS>
2021-03-31 12:48:10 -0400

    Updated driver to use only two complex numbers and display all operations.

--- rimplex/src/app/RimplexDriver.java
+++ rimplex/src/app/RimplexDriver.java
@@ -22,31 +22,35 @@ public class RimplexDriver
 
   /**
    * main method.
-   * @param args String[]
+   * 
+   * @param args
+   *          String[]
    */
   public static void main(String[] args)
   {
-    javax.swing.SwingUtilities.invokeLater(new Runnable() {
-      public void run() {
+    javax.swing.SwingUtilities.invokeLater(new Runnable()
+    {
+      public void run()
+      {
         // create GUI
         GuiContainer container = GuiContainer.createInstance();
         container.showGUI();
-        
+
         // get display
         DisplayComponent display = container.getDisplay();
-        
+
         // get input field
         InputField inputField = container.getInputField();
-        
+
         // insert Complex Numbers
         Random r = new Random();
-        for (int i = 0; i < r.nextInt(20) + 2; i++)
+        for (int i = 0; i < 2; i++)
         {
           double a = r.nextInt(100);
           double b = r.nextInt(100);
           complexNumbers.add(new ComplexNumber(a, b));
         }
-        
+
         // display Complex Numbers
         for (ComplexNumber num : complexNumbers)
         {
@@ -61,23 +65,100 @@ public class RimplexDriver
           }
         }
         display.addText(" = ");
-        
+
         // calculate result
         ComplexNumber result = complexNumbers.get(0);
         for (int i = 1; i < complexNumbers.size(); i++)
         {
           result = Operation.add(result, complexNumbers.get(i));
         }
-        
+
         // display result
         display.addComplexNumber(result);
-        
+        display.addText("\n\n");
+
+        // display Complex Numbers
+        for (ComplexNumber num : complexNumbers)
+        {
+          if (complexNumbers.indexOf(num) == complexNumbers.size() - 1)
+          {
+            display.addComplexNumber(num);
+          }
+          else
+          {
+            display.addComplexNumber(num);
+            display.addText(" - ");
+          }
+        }
+        display.addText(" = ");
+
+        // calculate result
+        result = complexNumbers.get(0);
+        for (int i = 1; i < complexNumbers.size(); i++)
+        {
+          result = Operation.subtract(result, complexNumbers.get(i));
+        }
+
+        // display result
+        display.addComplexNumber(result);
+        display.addText("\n\n");
+
+        for (ComplexNumber num : complexNumbers)
+        {
+          if (complexNumbers.indexOf(num) == complexNumbers.size() - 1)
+          {
+            display.addComplexNumber(num);
+          }
+          else
+          {
+            display.addComplexNumber(num);
+            display.addText(" * ");
+          }
+        }
+        display.addText(" = ");
+
+        // calculate result
+        result = complexNumbers.get(0);
+        for (int i = 1; i < complexNumbers.size(); i++)
+        {
+          result = Operation.multiply(result, complexNumbers.get(i));
+        }
+
+        // display result
+        display.addComplexNumber(result);
+        display.addText("\n\n");
+
+        for (ComplexNumber num : complexNumbers)
+        {
+          if (complexNumbers.indexOf(num) == complexNumbers.size() - 1)
+          {
+            display.addComplexNumber(num);
+          }
+          else
+          {
+            display.addComplexNumber(num);
+            display.addText(" / ");
+          }
+        }
+        display.addText(" = ");
+
+        // calculate result
+        result = complexNumbers.get(0);
+        for (int i = 1; i < complexNumbers.size(); i++)
+        {
+          result = Operation.divide(result, complexNumbers.get(i));
+        }
+
+        // display result
+        display.addComplexNumber(result);
+        display.addText("\n\n");
+
         // transfer text from inputField to display after pressing enter
         inputField.enterText(display, display.getText());
-        
+
         // set all i characters in display to italics
         display.displayTypesetting(0, display.getText().length() - 1);
       }
-  });
+    });
   }
 }
