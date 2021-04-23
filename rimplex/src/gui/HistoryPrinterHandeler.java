017d72d4471b127e68f4a01dd9fb2f33bd2a5009
vaporbolt <vaporbolt@gmail.com>
2021-04-23 13:09:16 -0400

    Added printing and print dialogue functionality to the print history button

---
+++ rimplex/src/gui/HistoryPrinterHandeler.java
@@ -0,0 +1,58 @@
+package gui;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.print.PageFormat;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+
+import printing.Printer;
+
+/**
+ * @author Seth Roper
+ * @version 4/23/21
+ * A listener for the printHistory button that opens a print dialogue for the history.
+ *
+ */
+public class HistoryPrinterHandeler implements ActionListener
+{
+  
+  private Component comp;
+  
+  /**
+   * @param comp the componet to be printed.
+   */
+  public HistoryPrinterHandeler(final Component comp)
+  {
+    this.comp = comp;
+  }
+
+  @Override
+  public void actionPerformed(final ActionEvent e)
+  {
+    PrinterJob pjob = PrinterJob.getPrinterJob();
+    PageFormat preformat = pjob.defaultPage();
+    preformat.setOrientation(PageFormat.LANDSCAPE);
+    PageFormat postformat = pjob.pageDialog(preformat);
+    // If user does not hit cancel then print.
+    if (preformat != postformat)
+    {
+      // Set print component
+      pjob.setPrintable(new Printer(this.comp), postformat);
+      if (pjob.printDialog())
+      {
+        try
+        {
+          pjob.print();
+        }
+        catch (PrinterException e1)
+        {
+          // printing failed.
+          e1.printStackTrace();
+        }
+      }
+    }
+  }
+
+}
